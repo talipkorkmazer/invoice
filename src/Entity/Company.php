954681ad35b6adaeed8f512fc343f648dda6069b
Talip Korkmazer <27894018+tkorkmazer@users.noreply.github.com>
2021-09-17 19:50:34 +0300

    composer

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -2,14 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\CompanyRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=CompanyRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
  */
 class Company
 {
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -27,32 +27,62 @@ class Company
      */
     private $address;
 
+
+    /**
+     * @return integer|null
+     */
     public function getId(): ?int
     {
         return $this->id;
-    }
 
+    }//end getId()
+
+
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
-    }
 
+    }//end getName()
+
+
+    /**
+     * @param  string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
 
         return $this;
-    }
 
+    }//end setName()
+
+
+    /**
+     * @return string|null
+     */
     public function getAddress(): ?string
     {
         return $this->address;
-    }
 
+    }//end getAddress()
+
+
+    /**
+     * @param string $address
+     *
+     * @return $this
+     */
     public function setAddress(string $address): self
     {
         $this->address = $address;
 
         return $this;
-    }
-}
+
+    }//end setAddress()
+
+
+}//end class
